9c842bcbac284241dddc42d5b33d417a555bd03c
지민 백 <s2104@e-mirim.hs.kr>
2022-11-11 09:52:20 +0900

    학습모델 구조(인공신경망 2개)

--- script.js
+++ script.js
@@ -16,7 +16,25 @@ async function getData() {
   return cleaned;
 }
 
-async function run() {
+//여기에 코드 붙이기
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential();
+
+  // 이 학습모델에는 인공 신경망이 2개 있음
+  // 인공 신경망이 3개 이상이어야 딥러닝으로 볼 수 있음
+  // Add a single input layer //인공 신경망1
+  model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+
+  // Add an output layer //인공 신경망2
+  model.add(tf.layers.dense({units: 1, useBias: true}));
+
+  return model;
+}
+
+
+
+async function run() { //메인함수
   // Load and plot the original input data that we are going to train on.
   const data = await getData();
   const values = data.map(d => ({
@@ -35,6 +53,10 @@ async function run() {
   );
 
   // More code will be added below
+  // Create the model
+  const model = createModel();
+  tfvis.show.modelSummary({name: 'Model Summary'}, model);
 }
 
 document.addEventListener('DOMContentLoaded', run);
+
